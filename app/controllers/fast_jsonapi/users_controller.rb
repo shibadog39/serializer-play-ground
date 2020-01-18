b3c063be2b930e732ddc7a5a0957debefca1ac81
櫻井 貴央 <shibadog1121@gmail.com>
2020-01-18 23:36:43 +0900

    postsのjsonの形が不正だったので修正する

--- app/controllers/fast_jsonapi/users_controller.rb
+++ app/controllers/fast_jsonapi/users_controller.rb
@@ -1,6 +1,6 @@
 class FastJsonapi::UsersController < ApplicationController
   def index
     users = User.all
-    render json: FastJsonapi::UsersSerializer.new(users).serialized_json
+    render json: FastJsonapi::UsersSerializer.new(users).serializable_hash
   end
 end
